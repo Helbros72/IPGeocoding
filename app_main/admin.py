dbe0919abaf52bf2e0e2a2166b8906804379318b
Oleg <helbros@gmail.com>
2024-05-27 21:42:15 +0300

    Adding admin settings

--- app_main/admin.py
+++ app_main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_main.models import History, Country
+
+#admin.site.register(History)
+admin.site.register(Country)
+
+@admin.register(History)
+class HistoryAdmin(admin.ModelAdmin):
+    list_display = ('id','ip','date','country')
+    search_fields = ('country',)
+    list_filter = ('date',)
+
+
+
